1b995d5154ca7b8ef750a3e8dd0afedbbf748172
HaQT00R <40822691+sullaspqr@users.noreply.github.com>
2025-11-10 09:29:53 +0100

    Translate text in App.jsx to Hungarian
    Updated text to Hungarian for localization.

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,14 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          a számláló állása: {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Módosítsd az <code>src/App.jsx</code>-t és mentsd le, hogy tesztelhesd a HMR-t!
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+         Kattints a React, a Kandó, vagy a Vite logókra, további információkért!
       </p>
     </>
   )
